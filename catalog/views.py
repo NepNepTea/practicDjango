7801ae095dc7fc510b28b744cf41dd470f5b2fac
Сергеев Артём Денисович <gr431_seade@ttit.local>
2024-11-19 13:30:10 +0700

    adding number of visits

--- catalog/views.py
+++ catalog/views.py
@@ -2,25 +2,22 @@ from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
 
-# Create your views here.
+
 def index(request):
-    """
-    Функция отображения для домашней страницы сайта.
-    """
-    # Генерация "количеств" некоторых главных объектов
     num_books=Book.objects.all().count()
     num_instances=BookInstance.objects.all().count()
-    # Доступные книги (статус = 'a')
+
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
-    num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
+    num_authors=Author.objects.count()
     num_genres=Genre.objects.count()
 
-    # Отрисовка HTML-шаблона index.html с данными внутри
-    # переменной контекста context
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genres':num_genres},
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genres':num_genres, 'num_visits':num_visits},
     )
 
 class BookListView(generic.ListView):
