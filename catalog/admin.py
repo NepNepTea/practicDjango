e9fe722305c2c04a05f65a3fc5170ca5a71186f7
Сергеев Артём Денисович <gr431_seade@ttit.local>
2024-11-19 10:05:21 +0700

    admin interface update

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,17 +6,34 @@ from .models import Author, Genre, Book, BookInstance
 admin.site.register(Genre)
 #admin.site.register(BookInstance)
 
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+class BooksInline(admin.TabularInline):
+    model = Book
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
 
 admin.site.register(Author, AuthorAdmin)
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book','imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
