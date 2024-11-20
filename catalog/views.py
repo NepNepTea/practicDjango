ca145eb3f03d5a3f1bb5320862c4282566e7b2d7
Сергеев Артём Денисович <gr431_seade@ttit.local>
2024-11-20 09:46:26 +0700

    add author and book create, delete, update

--- catalog/views.py
+++ catalog/views.py
@@ -9,6 +9,9 @@ from django.urls import reverse
 import datetime
 from django.contrib.auth.decorators import permission_required
 from .forms import RenewBookForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Author
 
 def index(request):
     num_books=Book.objects.all().count()
@@ -86,3 +89,28 @@ def renew_book_librarian(request, pk):
         form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
 
     return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst':book_inst})
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = '__all__'
+    initial={'date_of_death':'12/10/2016',}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name','last_name','date_of_birth','date_of_death']
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
+
+class BookCreate(CreateView):
+    model = Book
+    fields = '__all__'
+
+class BookUpdate(UpdateView):
+    model = Book
+    fields = ['title','author','summary','genre']
+
+class BookDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('books')
